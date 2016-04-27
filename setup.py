6e1408add93ad3410abe18fb45d68365dd712d1b
Lars Schöning <lars@lyschoening.de>
2016-04-27 13:21:31 +0200

    feat: add classifiers, install_requires to setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,24 @@ setup(
     description='Goodbye, GenBank is a package for use with Biopython that gives feature annotations from '
                 'GenBank records a new and better life.',
     long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    install_requires=[
+        'biopython>=1.66',
+        'six>=1.8.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ]
 )
